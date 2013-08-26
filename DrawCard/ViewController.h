f2d433de1a7764487e0e25b8c0091dded1fd810f
ffmaer <tz348@nyu.edu>
2013-08-26 15:06:28 -0400

    Initial Commit

---
+++ DrawCard/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrawCard
+//
+//  Created by tcz on 8/26/13.
+//  Copyright (c) 2013 Maer Studios Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DrawCard/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DrawCard
+//
+//  Created by tcz on 8/26/13.
+//  Copyright (c) 2013 Maer Studios Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
